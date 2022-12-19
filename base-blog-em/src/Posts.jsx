3680b487a05730418e401616934ef454f2988889
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-20 00:03:21 +0900

    chore : 폴더 이동

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0");
   return response.json();
 }
 
@@ -15,17 +14,17 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+
+  //두 번째 인수는 데이터를 가져오는 비동기 함수여야 한다.
+  const { data } = useQuery("posts", fetchPosts);
+  console.log(data);
 
   return (
     <>
       <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+        {/* fetchPosts 함수는 비동기이기 때문에 데이터가 들어오기 전까지는 빈 배열이다. */}
+        {data?.map((post) => (
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
