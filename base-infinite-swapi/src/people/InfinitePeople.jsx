527dbf287aa7be2e28eac924babeb00ee8b36ded
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-29 10:39:06 +0900

    useInfiniteQuery with starwars api

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,40 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
+
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetching, isError, error } =
+    useInfiniteQuery("sw-people", ({ pageParam = initialUrl }) => fetchUrl(pageParam), {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    });
+  if (isLoading) <span className="loading">Loading...</span>;
+
+  if (isError) <span>{error.toString()}</span>;
+
+  return (
+    <>
+      {isFetching ? <span className="loading">Fetching...</span> : null}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data?.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
